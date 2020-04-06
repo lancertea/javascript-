bce16a872f96be7732bdee6659ea37103d14eadb
lancer <43966458+lancertea@users.noreply.github.com>
2020-04-06 21:44:26 +0800

    update

--- ES6/parameter.js
+++ ES6/parameter.js
@@ -12,27 +12,55 @@
     }
     return x + y + z
   }
-  console.log(f(1, 3));
-} {
+console.log(f(1));//50
+console.log(f(1, 3));//46
+} 
+
+{
+function a(x,y){
+  x=x||1;
+  y=y||2;
+  return x*y;
+  }
+console.log(a(2));//4
+
+}
+
+
+
+{
   // ES6 默认参数
   function f(x, y = 7, z = 42) {
     return x + y + z
   }
-  console.log(f(1, 3));
-} {
+  console.log(f(1, 3));//46
+}
+
+// 必填参数的检查
+{
   function checkParameter() {
     throw new Error('can\'t be empty')
   }
   function f(x = checkParameter(), y = 7, z = 42) {
     return x + y + z
   }
-  console.log(f(1));
+  console.log(f(1));//50
   try {
     f()
   } catch (e) {
     console.log(e);
   } finally {}
-} {
+} 
+//可选参数
+{
+function sum(x,y,{m,n}={}){
+  console.log(x,y,m,n); 
+}
+sum(1,2);//1 2 undefined undefined
+}
+
+{
+// 不确定参数有几个，但是要对他们求和  
   // ES3,ES5 可变参数
   function f() {
     var a = Array.prototype.slice.call(arguments);
@@ -42,8 +70,10 @@
     })
     return sum
   }
-  console.log(f(1, 2, 3, 6));
-} {
+  console.log(f(1, 2, 3, 6));//12
+}
+
+{
   // ES6 可变参数
   function f(...a) {
     var sum = 0;
@@ -52,17 +82,21 @@
     });
     return sum
   }
-  console.log(f(1, 2, 3, 6));
-} {
+  console.log(f(1, 2, 3, 6));//12
+} 
+
+{
   // ES5 合并数组
   var params = ['hello', true, 7];
   var other = [1, 2].concat(params);
-  console.log(other);
-} {
+  console.log(other);//[ 1, 2, "hello", true, 7 ]
+} 
+
+{
   // ES6 利用扩展运算符合并数组
   var params = ['hello', true, 7];
   var other = [
     1, 2, ...params
   ];
-  console.log(other);
+  console.log(other);//[ 1, 2, "hello", true, 7 ]
 }
