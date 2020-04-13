d8ac1b8b4ea595f4118df265ee109606f64a04a8
lancer <43966458+lancertea@users.noreply.github.com>
2020-04-13 16:24:54 +0800

    update

--- DOM/dom-1.js
+++ DOM/dom-1.js
@@ -15,18 +15,19 @@
 
 
 //获取属性
-// const pList = document.querySelectorAll('p')
-// const p1 = pList[0]
+const pList = document.querySelectorAll('p')
+const p1 = pList[0]
 
-// // property 形式
-// p1.style.width = '100px'
-// console.log( p1.style.width )
-// p1.className = 'red'
-// console.log( p1.className )
-// console.log(p1.nodeName)
-// console.log(p1.nodeType) // 1
+// property 形式
+p1.style.width = '100px'
+console.log( p1.style.width )
+p1.style.color='blue'
+p1.className = 'red'
+console.log( p1.className )
+console.log(p1.nodeName)
+console.log(p1.nodeType) // 1
 
-// // attribute
+// attribute
 // p1.setAttribute('data-name', 'imooc')
 // console.log( p1.getAttribute('data-name') )
 // p1.setAttribute('style', 'font-size: 50px;')
