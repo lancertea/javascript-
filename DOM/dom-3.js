1fbf9cb2442a6af4128acfafc836dad0da1abd5b
lancer <43966458+lancertea@users.noreply.github.com>
2020-04-15 15:17:34 +0800

    Update dom-3.js

--- DOM/dom-3.js
+++ DOM/dom-3.js
@@ -1,15 +1,15 @@
 // // DOM查询做缓存
-// //不缓存DOM查询结果
-// for(let i=0;i<document.getElementsByTagName('p').length;i++){
-//     //每次循环，都会计算length，频繁进行DOM查询
-// }
+//不缓存DOM查询结果
+for(let i=0;i<document.getElementsByTagName('p').length;i++){
+    //每次循环，都会计算length，频繁进行DOM查询
+}
 
-// //缓存DOM查询结果
-// const pList =document.getElementsByTagName('p')
-// const length =pList.length
-// for(let i=0;i<length;i++){
-//     //缓存length,只进行一次DOM查询
-// }
+//缓存DOM查询结果
+const pList =document.getElementsByTagName('p')
+const length =pList.length
+for(let i=0;i<length;i++){
+    //缓存length,只进行一次DOM查询
+}
 
 
 //将频繁操作改为一次性操作
