12a55a895ff3add18dd821fe4f98a10b558fd25c
lancertea <864798435@qq.com>
2020-10-10 15:25:19 +0800

    许愿T.T

---
+++ ES6/module_ex.js
@@ -0,0 +1,19 @@
+//1
+// export const name = 'zzz'
+
+
+//2
+// const name ='zzz';
+// let addr = 'beijing';
+// let list =[1,2,3];
+
+// export default name
+// export {
+//   addr,
+//   list
+// }
+
+//3
+export function say(context){
+  console.log(context);
+}
---
+++ ES6/module_im.js
@@ -0,0 +1,10 @@
+//1
+// import {name,addr, list} from './module_ex.js'
+// console.log(name);
+
+//2
+// import name2, {addr as addr2}from './module_ex.js';
+// console.log(name2,addr2);
+
+import {say} from './module_ex.js'
+say('come on');
